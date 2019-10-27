3c3b38fafa3afb1baab2c40fec6df845951933f1
Kononenko Sergey <sergkononenko@mail.ru>
2019-10-27 20:09:26 +0300

    Add simple create_issue worker to test API

--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,28 @@
 """ Worker for IU7Games project. """
 
 
+import gitlab
+
+
+def create_issue(instance, project_id, title, description):
+    """ Create issue on GitLab instance. """
+    project = instance.projects.get(project_id)
+    project.issues.create(
+        {
+            'title': title,
+            'description': description
+        },
+        project_id=project_id)
+    print("Created Issue", title)
+
+
 def worker():
     """ API Worker to collect students' repos' artifacts. """
-    return True
+    gl = gitlab.Gitlab.from_config("gitiu7", ["./api_config.cfg"])
+    gl.auth()
+    create_issue(gl, 2546, "Тестовое issue, сгенерированное при помощи API",
+                 "Это issue было создано при помощи API.")
+
+
+if __name__ == "__main__":
+    worker()
