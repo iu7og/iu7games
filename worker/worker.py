b9ce94d66b4802d81421b665597ad427a31a32e1
Kononenko Sergey <sergkononenko@mail.ru>
2019-10-28 12:49:03 +0300

    Add Wiki handling utilities

--- worker/worker.py
+++ worker/worker.py
@@ -4,24 +4,52 @@
 import gitlab
 
 
-def create_issue(instance, project_id, title, description):
-    """ Create issue on GitLab instance. """
+def create_page(instance, project_id, title, content):
+    """ Create Wiki page. """
+
     project = instance.projects.get(project_id)
-    project.issues.create(
+    page = project.wikis.create(
         {
-            'title': title,
-            'description': description
-        },
-        project_id=project_id)
-    print("Created Issue", title)
+            "title": title,
+            "content": content
+        }
+    )
+
+
+def update_page(instance, project_id, page_slug, title, content):
+    """ Update Wiki page. """
+
+    project = instance.projects.get(project_id)
+    page = project.wikis.get(page_slug)
+    page.title = title
+    page.content = content
+    page.save()
+
+
+def delete_page(instance, project_id, page_slug):
+    """ Delete Wiki page. """
+
+    project = instance.projects.get(project_id)
+    page = project.wikis.get(page_slug)
+    page.delete()
 
 
 def worker():
     """ API Worker to collect students' repos' artifacts. """
+
+    games = {
+        "XOgame Leaderboard": "XOgame-Leaderboard",
+        "STRgame Leaderboard": "STRgame-Leaderboard",
+        "TEEN48game Leaderboard": "TEEN48game-Leaderboard"
+    }
+    games_keys = games.keys()
+    games_content = "Game Leaderboard"
+
     gl = gitlab.Gitlab.from_config("gitiu7", ["./api_config.cfg"])
     gl.auth()
-    create_issue(gl, 2546, "Тестовое issue, сгенерированное при помощи API",
-                 "Это issue было создано при помощи API.")
+
+    for key in games_keys:
+        create_page(gl, 2546, key, games_content)
 
 
 if __name__ == "__main__":
