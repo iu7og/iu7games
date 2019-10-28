7ef5842e3add4ead6375dadb2f6013b139b81efb
Kononenko Sergey <sergkononenko@mail.ru>
2019-10-28 19:59:58 +0300

    Add collect_artifacts function

--- worker/worker.py
+++ worker/worker.py
@@ -2,6 +2,8 @@
 
 
 import gitlab
+import os
+import subprocess
 
 
 def create_page(instance, project_id, title, content):
@@ -34,6 +36,17 @@ def delete_page(instance, project_id, page_slug):
     page.delete()
 
 
+def collect_artifacts(instance, project_id, job_id):
+    """ Collect job's artifacts. """
+
+    project = instance.projects.get(project_id)
+    job = project.jobs.get(job_id)
+
+    ziparts = str(project_id) + str(job_id) + "artifacts.zip"
+    with open(ziparts, "wb") as f:
+        job.artifacts(streamed=True, action=f.write)
+
+
 def worker():
     """ API Worker to collect students' repos' artifacts. """
 
@@ -45,11 +58,17 @@ def worker():
     games_keys = games.keys()
     games_content = "Game Leaderboard"
 
+    iu7games_id = 2546
+    iu7cprogsems_id = 2303
+
     gl = gitlab.Gitlab.from_config("gitiu7", ["./api_config.cfg"])
     gl.auth()
 
+    """
     for key in games_keys:
-        create_page(gl, 2546, key, games_content)
+        create_page(gl, iu7games_id, key, games_content)
+    collect_artifacts(gl, iu7cprogsems_id, 70250)
+    """
 
 
 if __name__ == "__main__":
