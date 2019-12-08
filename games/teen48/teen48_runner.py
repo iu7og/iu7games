b933401c702bfccc78c31ec8145a26ea8941ecf9
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2019-12-08 17:41:23 +0300

    add matrix filling / read in teen48 runner

--- games/teen48/teen48_runner.py
+++ games/teen48/teen48_runner.py
@@ -14,23 +14,30 @@ class matrix_t(ctypes.Structure):
         self.rows = rows
         self.columns = columns
 
+        c_int_p = ctypes.POINTER(ctypes.c_int)
+        value_array = ctypes.c_int * columns
+        pointer_array = c_int_p * rows
+        matrix_pointer = pointer_array()
+        for i in range(4):
+            matrix_pointer[i] = value_array()
+            for j in range(4):
+                matrix_pointer[i][j] = 0
+
+        self.matrix_ptr = matrix_pointer
+
+
 
 
 def start_teen48game_competition(players_info, field_size):
+    results = []
+
     for player in players_info:
         player_lib = ctypes.CDLL(player)
-        player_lib.teen48game.argtypes = [ctypes.POINTER(matrix_t)]
+        player_lib.teen48game.argtypes = [matrix_t]
         player_lib.teen48game.restype = ctypes.c_char
         matrix = matrix_t(field_size, field_size)
-        """
-        TODO: разобраться как нормально передать поинтер на поинтер, а не сишный массив....
-        ничё не работает..грустно....
-        """
-        #matrix.matrix_ptr = ctypes.cast(c, ctypes.POINTER(ctypes.POINTER(ctypes.c_int)))
-        #q = ctypes.cast(k, ctypes.POINTER((ctypes.c_int * 4)))
-        #matrix.matrix_ptr = ctypes.cast(b, ctypes.POINTER(ctypes.c_int))
-        
-        player_lib.teen48game(ctypes.byref(matrix))
+
+        player_lib.teen48game(matrix)
         for i in range(4):
             for j in range(4):
                 print(matrix.matrix_ptr[i][j])
