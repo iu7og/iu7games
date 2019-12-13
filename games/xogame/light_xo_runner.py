1b7c2455e5e4ed812028be6168a8ebbcb2ed9b23
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2019-12-13 20:06:25 +0300

    add xogame light runer

--- games/xogame/light_xo_runner.py
+++ games/xogame/light_xo_runner.py
@@ -5,3 +5,20 @@
       Данный ранер запускает функцию split игрока с тестовой стратегий,
       для проверки на отсутствие segmentation fault и бесконечный цикл в стратегии игрока.
 """
+
+from games.xogame.xo_runner import start_xogame_competition
+
+def light_xo_runner(player_lib_path):
+    """
+        Запуск стратегии игрока с тестовой стратегией на игровых полях 3х3 и 5х5.
+    """
+
+    start_xogame_competition([(player_lib_path, 0), ("/usr/lib/test_xo_lib.so", 0)], 3)
+    print("XO GAME: 3x3 FIELD OKAY")
+
+    start_xogame_competition([(player_lib_path, 0), ("/usr/lib/test_xo_lib.so", 0)], 5)
+    print("XO GAME: 5x5 FIELD OKAY")
+
+
+if __name__ == "__main__":
+    light_xo_runner("./test3.so")
