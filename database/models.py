9db7e1ae3c10d1c0fdfe8bc0fe5dd814f2ec49ce
Gregory Mironov <devgregorymironov@gmail.com>
2020-08-11 15:53:06 +0300

    Model classes declaration fixed

--- database/models.py
+++ database/models.py
@@ -14,8 +14,8 @@ class Player(mg.Document):
     """
 
     gitlab_id = mg.StringField(max_length=64, primary_key=True)
-    name: mg.StringField(max_length=64, required=True)
-    trackers: mg.DictField()
+    name = mg.StringField(max_length=64, required=True)
+    trackers = mg.DictField()
 
 
 class Achievement(mg.Document):
@@ -23,9 +23,9 @@ class Achievement(mg.Document):
         Класс, представляющий достижение игрока
     """
 
-    name: mg.StringField(max_length=32, required=True)
-    description: mg.StringField(max_length=128, required=True)
-    states: mg.DictField(required=True)
+    name = mg.StringField(max_length=32, required=True)
+    description = mg.StringField(max_length=128, required=True)
+    states = mg.DictField(required=True)
 
 
 class Game(mg.Document):
@@ -33,7 +33,7 @@ class Game(mg.Document):
         Класс, представляющий игру
     """
 
-    name: mg.StringField(required=True)
+    name = mg.StringField(required=True)
 
 
 class GameResult(mg.Document):
@@ -41,7 +41,7 @@ class GameResult(mg.Document):
         Класс, представляющий результат игрока в игре
     """
 
-    game: mg.ReferenceField(Game)
-    player: mg.ReferenceField(Player)
-    position: mg.IntField(min_value=0)
-    error_code: mg.IntField()
+    game = mg.ReferenceField(Game)
+    player = mg.ReferenceField(Player)
+    position = mg.IntField(min_value=0)
+    error_code = mg.IntField()
