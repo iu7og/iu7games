228b670a3e0d55b1e6c6100f30109f92578b6c26
Gregory Mironov <devgregorymironov@gmail.com>
2020-08-25 22:15:12 +0300

    Achievemet's data classes added instead of named tuples

--- database/achievements.py
+++ database/achievements.py
@@ -5,50 +5,37 @@
         Модуль с функциями для обработки достижений игроков
 """
 
-from collections import namedtuple
+from dataclasses import dataclass
 from functools import reduce
 from typing import List, Dict
 import re
 import mongoengine as mg
 import models
 
-AchievementList = namedtuple(
-    'ACHIEVEMENTS',
-    [
-        'STRATEGY_LOST',
-        'WRONG_RES',
-        'HABITUE',
-        'FIRST_AMONG_EQUALS',
-        'THE_NIGHT_IS_STILL_YOUNG',
-        'ALMOST_THERE',
-        'AUTOMERGE_KING'
-    ]
-)
 
-ACHIEVEMENTS = AchievementList(
-    STRATEGY_LOST='strategy_lost',
-    WRONG_RES='wrong_res',
-    HABITUE='habitue',
-    FIRST_AMONG_EQUALS='first_among_equals',
-    THE_NIGHT_IS_STILL_YOUNG='the_night_is_still_young',
-    ALMOST_THERE='almost_there',
-    AUTOMERGE_KING='automerge_king'
-)
+@dataclass
+class Achievement:
+    """
+        Класс с названиями достижений игрока
+    """
+    strategy_lost = 'strategy_lost'
+    wrong_res = 'wrong_res'
+    habitue = 'habitue'
+    first_among_equals = 'first_among_equals'
+    the_night_is_still_young = 'the_night_is_still_young'
+    almost_there = 'almost_there'
+    automerge_king = 'automerge_king'
 
-ErrorCodesList = namedtuple(
-    'ERROR_CODES',
-    [
-        'DEFAULT_VALUE',
-        'STRATEGY_LOST',
-        'WRONG_RES'
-    ]
-)
 
-ERROR_CODES = ErrorCodesList(
-    DEFAULT_VALUE=0,
-    STRATEGY_LOST=-1,
-    WRONG_RES=-2
-)
+@dataclass
+class Error:
+    """
+        Класс с видами ошибочных результатов игрока
+    """
+    default_value = 0
+    strategy_lost = -1
+    wrong_res = -2
+
 
 ERROR_REGEX = r'^[а-яА-Я ]+$'
 WRONG_RES_REGEX = r'^\[\d\]+$'
@@ -67,43 +54,43 @@ def add_achievements_to_db() -> None:
     models.Achievement(
         name='И так сойдет...',
         description='Получить статус "Отсутствует стратегия" в лидерборде одной из игр',
-        states={ACHIEVEMENTS.STRATEGY_LOST: 1}
+        states={Achievement.strategy_lost: 1}
     ).save()
 
     models.Achievement(
         name='Но у меня работало...',
         description='Получить статус "[0]" в лидерборде одной из игр',
-        states={ACHIEVEMENTS.WRONG_RES: 1}
+        states={Achievement.wrong_res: 1}
     ).save()
 
     models.Achievement(
         name='Завсегдатай...',
         description='Во всех играх есть рабочая стратегия',
-        states={ACHIEVEMENTS.HABITUE: 5}
+        states={Achievement.habitue: 5}
     ).save()
 
     models.Achievement(
         name='Почти получилось',
         description='Занять 4-ое место в одной из игр',
-        states={ACHIEVEMENTS.ALMOST_THERE: 1}
+        states={Achievement.almost_there: 1}
     ).save()
 
     models.Achievement(
         name='Ещё не вечер',
         description='Занять 2-ое или 3-е место в одной из игр',
-        states={ACHIEVEMENTS.THE_NIGHT_IS_STILL_YOUNG: 1}
+        states={Achievement.the_night_is_still_young: 1}
     ).save()
 
     models.Achievement(
         name='Первый среди равных',
         description='Занять 1-ое место в одной из игр',
-        states={ACHIEVEMENTS.FIRST_AMONG_EQUALS: 1}
+        states={Achievement.first_among_equals: 1}
     ).save()
 
     models.Achievement(
         name='Король автомержей',
         description='Войти в тройку во всех играх',
-        states={ACHIEVEMENTS.ALMOST_THERE: 5}
+        states={Achievement.automerge_king: 5}
     ).save()
 
     mg.disconnect()
@@ -139,11 +126,11 @@ def update_players_results(game_name: str, results: List[List[str]]) -> None:
         player_res.position = result[0]
 
         if re.match(ERROR_REGEX, result[4]):
-            player_res.error_code = ERROR_CODES.STRATEGY_LOST
+            player_res.error_code = Error.strategy_lost
         elif re.match(WRONG_RES_REGEX, result[4]):
-            player_res.error_code = ERROR_CODES.WRONG_RES
+            player_res.error_code = Error.wrong_res
         else:
-            player_res.error_code = ERROR_CODES.DEFAULT_VALUE
+            player_res.error_code = Error.default_value
 
         player_res.save()
 
@@ -174,16 +161,16 @@ def update_players_trackers(game_name: str, users: List[List[str]]) -> None:
                             game else y, results).error_code
 
         # И так сойдет
-        if error_code == ERROR_CODES.STRATEGY_LOST:
-            update_tracker(player, ACHIEVEMENTS.STRATEGY_LOST, 1)
+        if error_code == Error.strategy_lost:
+            update_tracker(player, Achievement.strategy_lost, 1)
         # Но у меня работало
-        elif error_code == ERROR_CODES.WRONG_RES:
-            update_tracker(player, ACHIEVEMENTS.WRONG_RES, 1)
+        elif error_code == Error.wrong_res:
+            update_tracker(player, Achievement.wrong_res, 1)
 
         positions = {'1': 0, '1_2_3': 0, '2_3': 0, '4': 0, 'other': 0}
         for res in results:
 
-            if res.error_code != ERROR_CODES.DEFAULT_VALUE:
+            if res.error_code != Error.default_value:
                 continue
 
             if res.position == 0:
@@ -201,21 +188,21 @@ def update_players_trackers(game_name: str, users: List[List[str]]) -> None:
             positions['other'] += 1
 
         # Завсегдатай
-        update_tracker(player, ACHIEVEMENTS.HABITUE, positions['other'])
+        update_tracker(player, Achievement.habitue, positions['other'])
 
         # Почти получилось
-        update_tracker(player, ACHIEVEMENTS.ALMOST_THERE,
+        update_tracker(player, Achievement.almost_there,
                        1 if positions['4'] > 0 else 0)
 
         # Ещё не вечер
-        update_tracker(player, ACHIEVEMENTS.THE_NIGHT_IS_STILL_YOUNG,
+        update_tracker(player, Achievement.the_night_is_still_young,
                        1 if positions['2_3'] > 0 else 0)
 
         # Король автомержей
-        update_tracker(player, ACHIEVEMENTS.AUTOMERGE_KING, positions['1_2_3'])
+        update_tracker(player, Achievement.automerge_king, positions['1_2_3'])
 
         # Первый среди равных
-        update_tracker(player, ACHIEVEMENTS.FIRST_AMONG_EQUALS,
+        update_tracker(player, Achievement.first_among_equals,
                        1 if positions['1'] > 0 else 0)
 
     mg.connect()
